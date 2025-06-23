0cfa0412323fbac487de1cbc32e460821e166cc5
e227474 <e227474@users.noreply.github.com>
2025-06-23 14:54:52 +0200

    minor level changes

--- tiled/tileset.tsx
+++ tiled/tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="8" columns="4">
- <image source="../public/assets/tileset.png" width="136" height="68"/>
+<tileset version="1.10" tiledversion="1.11.1" name="tileset" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="12" columns="4">
+ <image source="../public/assets/tileset.png" width="136" height="102"/>
  <tile id="1">
   <properties>
    <property name="collides" type="bool" value="true"/>
@@ -17,6 +17,26 @@
   </properties>
  </tile>
  <tile id="7">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
